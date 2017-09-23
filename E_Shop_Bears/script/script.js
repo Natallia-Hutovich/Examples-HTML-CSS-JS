e901093b57fa9ca944e2b706df70bee669b24bcf
Natallia-Hutovich <31992627+Natallia-Hutovich@users.noreply.github.com>
2017-09-23 13:48:28 +0300

    Add files via upload

---
+++ E_Shop_Bears/script/script.js
@@ -0,0 +1,437 @@
+﻿var big_arr=
+[
+{
+	"id" : "bears001",
+	"full_name" : "Медведь 'Гриня140'",
+	"size" : 140,
+	"img" : "200/grinya140.png",
+	"price" : 68,
+	"size_category" : "110-200"
+	
+},
+{
+	"id" : "bears002",
+	"full_name" : "Медведь 'Изюм140'",
+	"size" : 140,
+	"img" : "200/izum140.png",
+	"price" : 65,
+	"size_category" : "110-200"
+	
+},
+{
+	"id" : "bears003",
+	"full_name" : "Медведь 'Мармелад огромный'",
+	"size" : 160,
+	"img" : "200/marmelad_big160.png",
+	"price" : 73,
+	"size_category" : "110-200"
+	
+},
+{
+	"id" : "bears026",
+	"full_name" : "Медведь 'Ника130'",
+	"size" : 130,
+	"img" : "200/nika130.png",
+	"price" : 68,
+	"size_category" : "110-200"
+	
+},
+{
+	"id" : "bears004",
+	"full_name" : "Медведь 'Тэдди210'",
+	"size" : 210,
+	"img" : "300/teddi210.png",
+	"price" : 115,
+	"size_category" : "200-300"
+	
+},
+
+{
+	"id" : "bears005",
+	"full_name" : "Медведь 'Самсон210'",
+	"size" : 210,
+	"img" : "300/samson210.png",
+	"price" : 115,
+	"size_category" : "200-300"
+	
+},
+
+{
+	"id" : "bears006",
+	"full_name" : "Медведь 'Захар фиол.'",
+	"size" : 226,
+	"img" : "300/zahar226.png",
+	"price" : 120,
+	"size_category" : "200-300"
+	
+},
+
+{
+	"id" : "bears007",
+	"full_name" : "Медведь 'Гарри коричн. 213'",
+	"size" : 213,
+	"img" : "300/garry213.png",
+	"price" : 89,
+	"size_category" : "200-300"
+	
+},
+
+{
+	"id" : "bears008",
+	"full_name" : "Медведь 'Гриня чайн. 210'",
+	"size" : 210,
+	"img" : "300/grinya210.png",
+	"price" : 110,
+	"size_category" : "200-300"
+	
+},
+
+{
+	"id" : "bears009",
+	"full_name" : "Медведь коричневый 46",
+	"size" : 46,
+	"img" : "110/med_brown46.png",
+	"price" : 20,
+	"size_category" : "40-110"
+	
+},
+
+{
+	"id" : "bears010",
+	"full_name" : "Медведь коричневый 76",
+	"size" : 76,
+	"img" : "110/med_brown76.png",
+	"price" : 30,
+	"size_category" : "40-110"
+	
+},
+
+{
+	"id" : "bears011",
+	"full_name" : "Медведь коричневый 92",
+	"size" : 92,
+	"img" : "110/med_brown92.png",
+	"price" : 40,
+	"size_category" : "40-110"
+	
+},
+
+{
+	"id" : "bears012",
+	"full_name" : "Медведь белый сидящий 25",
+	"size" : 25,
+	"img" : "40/med_white25.png",
+	"price" : 15,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears013",
+	"full_name" : "Медведь кремовый",
+	"size" : 28,
+	"img" : "40/med_crem28.png",
+	"price" : 17,
+	"size_category" : "0-40"
+	
+},
+
+{	
+	"id" : "bears014",
+	"full_name" : "Медведь Захар 28",
+	"size" : 28,
+	"img" : "40/med_zahar28.png",
+	"price" : 15,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears015",
+	"full_name" : "Медведь Саня 26",
+	"size" : 26,
+	"img" : "40/med_sanya26.png",
+	"price" : 15,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears016",
+	"full_name" : "Медведь с сердцем 18",
+	"size" : 18,
+	"img" : "40/med_heart18.png",
+	"price" : 10,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears017",
+	"full_name" : "Медведь с сердцем коричневый 20",
+	"size" : 20,
+	"img" : "40/med_heart20.png",
+	"price" : 11,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears018",
+	"full_name" : "Медведь в футболке коричневый 26",
+	"size" : 26,
+	"img" : "40/med_tshirt20.png",
+	"price" : 20,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears019",
+	"full_name" : "Медведь с сердцем 27",
+	"size" : 27,
+	"img" : "40/med_heart27.png",
+	"price" : 26,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears020",
+	"full_name" : "Медведь 30",
+	"size" : 30,
+	"img" : "40/med30.png",
+	"price" : 24,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears021",
+	"full_name" : "Медведь 28",
+	"size" : 28,
+	"img" : "40/med28.png",
+	"price" : 24,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears022",
+	"full_name" : "Медведь 17",
+	"size" : 17,
+	"img" : "40/med17.png",
+	"price" : 15,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears023",
+	"full_name" : "Медведь в шапке 25",
+	"size" : 25,
+	"img" : "40/med_cap25.png",
+	"price" : 19,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears024",
+	"full_name" : "Медведь 32",
+	"size" : 32,
+	"img" : "40/med32.png",
+	"price" : 27,
+	"size_category" : "0-40"
+	
+},
+
+{
+	"id" : "bears025",
+	"full_name" : "Медведь в шапке 28",
+	"size" : 28,
+	"img" : "40/med_cap28.png",
+	"price" : 35,
+	"size_category" : "0-40"
+	
+}
+];
+window.onload = function() {
+function render_table(arr,table){
+		table.innerHTML='';
+		var temp=table.parentNode.querySelector('.card_template');
+		arr.forEach(function(item){
+			var new_temp=temp.cloneNode(true);
+			new_temp.classList.remove('card_template');
+			table.appendChild(new_temp);
+			new_temp.setAttribute('data-id',item["id"]);
+			new_temp.setAttribute('data-size',item["size_category"]);
+			var img=new_temp.querySelector('.card_img img');
+			img.setAttribute('src','img/'+item['img']);
+			var name=new_temp.querySelector('.card_name');
+			name.innerText=item['full_name'];
+			var size=new_temp.querySelector('.card_size');
+			size.innerText=item['size']+'см';
+			var price=new_temp.querySelector('.card_price');
+			price.innerText=item['price']+' руб.';
+		})
+		temp=table.querySelectorAll('.card_name');
+		var maxHeight=0;
+		temp.forEach(function(item){
+			var height=parseFloat(getComputedStyle(item).height);
+			if(height>maxHeight){
+				maxHeight=height;
+			}	
+		});
+		temp.forEach(function(item){
+			item.style.height=maxHeight+'px';
+		});	
+	}
+	 
+	
+	function render_cart(cartId,arr){
+		var cart_count=document.getElementById(cartId+'_count');	
+		var cart_sum=document.getElementById(cartId+'_sum');	
+		var count=0;
+		var sum=0;
+		arr.forEach(function(item){
+			count+=item.count;
+			sum+=item.price;
+		});
+		cart_count.innerText=count;
+		cart_sum.innerText=sum;
+	}
+	
+	function get_cart(){
+		var arr=[];
+		if (localStorage.getItem(LOCAL_VALUE)){
+			arr=JSON.parse(localStorage.getItem(LOCAL_VALUE));
+		}
+		return arr;
+	}
+	 
+	function set_cart(cartId,newElem){
+		var arr=get_cart();
+		var inArr=false;
+		arr.forEach(function(item){
+			if(item.id===newElem.id){
+				item.count+=newElem.count;
+				item.price+=newElem.price;
+				inArr=true;
+			}	
+		});
+		if (!inArr){arr.push(newElem);}
+		localStorage.setItem(LOCAL_VALUE, JSON.stringify(arr));
+		render_cart(cartId,arr);
+	} 
+	 
+	 function handler_table(e){
+		 var target=e.target;
+		 var parent;
+		 var arr=[];
+		 var obj={};
+		if (target.tagName=='BUTTON'){
+			e.stopPropagation();
+			parent=target.closest('div.card');
+			obj.id=parent.getAttribute('data-id');
+			obj.count=1;
+			obj.price=parseFloat(parent.querySelector('.card_price').innerText);
+			set_cart(CART,obj);
+		}
+	}
+	 
+	 function handler_load(){
+		/* var arr;
+		 var xhr=new XMLHttpRequest();
+		 xhr.open('GET',FILE);
+		 xhr.onreadystatechange = function() {
+			 if ((xhr.readyState===4) && (xhr.status===200)) {
+				 console.log(xhr.responseText);
+				arr=JSON.parse(xhr.responseText);
+				render_table(arr,table);
+			}
+		}
+		 xhr.send();*/
+		render_table(big_arr,table);
+	 }
+	 
+	function hadler_cart(){
+		var arr=get_cart();
+		render_cart(CART,arr);
+	}	
+		
+	function filter_category(date,attribute,category){
+			
+		if (category==='all'){
+			for (var i=0;i<date.length;i++){
+				date[i].style.display='block';
+			}
+		}
+		else{
+			for (var i=0;i<date.length;i++){
+				var attr=date[i].getAttribute(attribute);
+				if (attr.indexOf(category)>-1){
+					date[i].style.display='block';
+				}
+				else{
+					date[i].style.display='none';
+					
+				}	
+			}	
+		}
+	}
+	
+	
+	function handler_menu_size(e){
+		 var target=e.target;
+		
+		 if (target.tagName=='A'){
+			e.preventDefault();
+			var size=target.getAttribute('data-size');
+			var size1='';
+			var size2='';
+			filter_category(table.querySelectorAll('.card'),'data-size',size);
+			var header=document.getElementById('card_block_header');
+			if (size=='all'){
+				header.innerText='Медведи все размеры';
+			}
+			else{
+				size1=size.slice(0,size.indexOf('-'));
+				size2=size.slice(size.indexOf('-')+1);
+				
+				if (size1=='0'){
+					header.innerText='Медведи до '+size2+'см';
+				}
+				else if (size2=='300'){
+					header.innerText='Медведи выше '+size1+'см';
+				}
+				else{
+					header.innerText='Медведи '+target.getAttribute('data-size')+'см';
+				}
+			}	
+		 }	
+	}
+	
+	function handler_aside(e){
+		 var target=e.target;
+		 if(target.classList.contains('menu_list')){
+			 var panel=target.nextElementSibling;
+			 panel.classList.toggle('hidden');
+			 target.classList.toggle('menu_list_active');
+		 }
+			
+	}
+	
+		var FILE='products.json?noCache='+(new Date().getTime());
+		//var FILE='products.json';
+		var LOCAL_VALUE='bears_card';	
+		var CART='cart';
+		var table=document.getElementById('products_card');
+		var menu_size=document.getElementById('menu_size');
+		var aside=document.querySelector('aside');
+		table.addEventListener('click',handler_table);
+		menu_size.addEventListener('click',handler_menu_size);
+		aside.addEventListener('click',handler_aside);
+		handler_load();
+		hadler_cart();
+  };
